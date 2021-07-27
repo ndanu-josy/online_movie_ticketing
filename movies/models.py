459c77bc8a2f0d82425d85a3e3128bd684c25cb2
ndanu-josy <josphinendanu3@gmail.com>
2021-07-27 18:49:21 +0300

    Add models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Theatre(models.Model):
+    city_choice=(
+        ('NAIROBI','nairobi'),
+        ('KISUMU','Kisumu'),
+        ('MOMBASA','Mombasa'),
+        
+    )
+    name = models.CharField(max_length=50,null=False,default="Waves Cinema")
+    city = models.CharField(max_length=20,choices=city_choice,null=False)
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name+"-"+self.address+"-"+self.city
+
+class Hall(models.Model):
+
+    HALL_TYPES = [
+    ('2D', '2D'),
+    ('3D', '3D'),
+    ('4DX', '4DX'),
+    ('IMAX', 'IMAX'),
+    ]
+
+    name = models.CharField(max_length=30)
+    hall_type = models.CharField(max_length=4, choices=HALL_TYPES)
+    theatre = models.ForeignKey(Theatre, on_delete=models.CASCADE)
+
+class Movie(models.Model):
+    name = models.CharField(max_length=30)
+    poster = CloudinaryField('imageposter')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
