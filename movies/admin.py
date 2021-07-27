161ecaec62a774359216f3f5ca3268617adee74d
ndanu-josy <josphinendanu3@gmail.com>
2021-07-27 18:52:13 +0300

    register models in admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import  Theatre, Hall, Movie
 
 # Register your models here.
+admin.site.register(Hall)
+admin.site.register(Movie)
+admin.site.register(Theatre)
